284b4df6f6d730dcfd96b0275d4d08d29a00edf5
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-08-21 10:33:32 +0200

    Add fetch and render code to heroes-list.tsx

--- diligent-academy-assessments/frontend/react-challenge/src/heroes-list.tsx
+++ diligent-academy-assessments/frontend/react-challenge/src/heroes-list.tsx
@@ -1,8 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { callApi } from "./call-api";
+import HeroItem from "./hero-item";
+
+type Hero = {
+  id: number;
+  name: string;
+  available: boolean
+}
+
+function fetchHeroes() {
+  return callApi<Hero[]>('heroes')
+    .then(heroesList => heroesList)
+    .catch(error => {
+      console.error("Error fetching heroes:", error);
+      return [];
+    });
+};
+
 function HeroesList() {
-  
+  const [heroes, setHeroes] = useState<Hero[]>([]);
+
+  useEffect(() => {
+    fetchHeroes().then(heroesList => {
+      setHeroes(heroesList);
+    });
+  }, []);
+
   return (
     <>
       <h2>Heroes</h2>
+      <ul>
+        {heroes.map(hero => (
+          <HeroItem key={hero.id} {...hero} />
+        ))}
+      </ul>
     </>
   );
 }
